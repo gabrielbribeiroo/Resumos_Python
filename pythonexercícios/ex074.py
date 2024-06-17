bf2f4d610ae9b314f71f99f046c29df03bda5324
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2024-06-17 12:03:54 -0300

    Delete pythonexercícios/ex074.py

--- pythonexercícios/ex074.py
+++
@@ -1,6 +0,0 @@
-# Números sorteados
-from random import randint
-número = (randint(1, 10), randint(1, 10), randint(1, 10), randint(1, 10), randint(1, 10))  # tupla formada por 5 números sorteados entre 1 e 10
-print(f'Eu sorteei os valores: {número}')
-print(f'O maior valor sorteado foi {max(número)}')  # função max encontra o máximo valor
-print(f'O menor valor sorteado foi {min(número)}')  # função min encontra o mínimo valor
