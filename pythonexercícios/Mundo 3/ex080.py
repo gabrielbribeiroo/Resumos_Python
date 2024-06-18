c78cccb1bef31acac3412845cf968f10ff5c4427
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2024-06-18 11:14:06 -0300

    ordered list without repetitions

---
+++ pythonexercícios/Mundo 3/ex080.py
@@ -0,0 +1,16 @@
+num = []
+for cont in range(0, 5):
+    n = int(input('Digite um valor: '))
+    if cont == 0 or n > num[-1]:
+        num.append(n)
+        print('Adicionado ao final da lista...')
+    else:
+        pos = 0
+        while pos < len(num):
+            if n <= num[pos]:
+                num.insert(pos, n)
+                print(f'Adicionado na posição {pos} da lista...')
+                break
+            pos += 1
+print('-='*30)
+print(f'Os valores digitados em ordem foram {num}')
