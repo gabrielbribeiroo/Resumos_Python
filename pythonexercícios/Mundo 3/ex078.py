c7e175a370fe467a30c86271fedd38634d8d8077
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2024-06-18 10:48:29 -0300

    highest and lowest value in the list

--- pythonexercícios/Mundo 3/ex078.py
+++ pythonexercícios/Mundo 3/ex078.py
@@ -1,2 +1,24 @@
-nome = str(input('Qual seu nome? '))
-print(f'É um prazer te conhecer, {nome.capitalize()}!')
+num = []
+maior = 0
+menor = 0
+for cont in range(0, 5):
+    num.append(int(input(f'Digite um valor para posição {cont}: ')))
+    if cont == 0:
+        maior = menor = num[cont]
+    else:
+        if num[cont] > maior:
+            maior = num[cont]
+        if num[cont] < menor:
+            menor = num[cont]
+print('-='*30)
+print(f'Você digitou os valores {num}')
+print(f'O maior valor digitado foi {maior} nas posições ', end='')
+for i, v in enumerate(num):
+    if v == maior:
+        print(f'{i}... ', end='')
+print()
+print(f'O menor valor digitado foi {menor} nas posições ', end='')
+for i, v in enumerate(num):
+    if v == menor:
+        print(f'{i}... ', end='')
+print()
