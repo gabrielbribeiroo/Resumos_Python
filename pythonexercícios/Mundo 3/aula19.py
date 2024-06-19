2b295882f617c429c842da9aa7010698558fe4f5
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2024-06-19 11:03:13 -0300

    dictionaries

---
+++ pythonexercícios/Mundo 3/aula19.py
@@ -0,0 +1,41 @@
+pessoas = {'nome': 'Gustavo', 'sexo': 'M', 'idade': 22}
+print(pessoas)
+print(pessoas['nome'])
+print(pessoas['idade'])
+print(f'O {pessoas["nome"]} tem {pessoas["idade"]} anos.')
+print(pessoas.keys())
+print(pessoas.values())
+print(pessoas.items())
+for k in pessoas.keys():
+    print(k)
+for v in pessoas.values():
+    print(v)
+del pessoas['sexo']
+pessoas['nome'] = 'Leandro'
+pessoas['peso'] = 98.5
+for k, v in pessoas.items():
+    print(f'{k} = {v}')
+print()
+
+brasil = []
+estado1 = {'uf': 'Rio de Janeiro', 'sigla': 'RJ'}
+estado2 = {'uf': 'São Paulo', 'sigla': 'SP'}
+brasil.append(estado1)
+brasil.append(estado2)
+print(estado1)
+print(estado2)
+print(brasil[0])
+print(brasil[1])
+print(brasil[0]['uf'])
+print(brasil[1]['sigla'])
+
+estado = dict()
+pais = list()
+for c in range(0, 3):
+    estado['uf'] = str(input('Unidade federativa: '))
+    estado['sigla'] = str(input('Sigla do estado: '))
+    pais.append(estado.copy())
+for e in pais:
+    for v in e.values():
+        print(v, end=' ')
+    print()
