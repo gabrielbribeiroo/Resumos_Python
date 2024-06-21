578ad16e5eab6120f514cbae31f5bd9e775d34aa
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2024-06-21 10:29:26 -0300

    aligned and formatted message

---
+++ pythonexercícios/Mundo 3/ex097.py
@@ -0,0 +1,9 @@
+def escreva(txt):
+    tam = len(txt) + 4
+    print('~'*tam)
+    print(f'  {txt}')
+    print('~'*tam)
+
+escreva('Gabriel Ribeiro')
+escreva('Curso de Python no YouTube')
+escreva('CeV')
