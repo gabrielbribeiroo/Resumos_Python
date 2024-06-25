330f26a3abfa5549f931175cde0d017fe8d77946
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2024-06-25 12:40:21 -0300

    system menu

---
+++ pythonexercícios/Mundo 3/ex115/sistema.py
@@ -0,0 +1,15 @@
+from lib.interface import *
+from time import sleep
+
+while True:
+    resposta = menu(['Ver pessoas cadastradas', 'Cadastrar nova pessoa', 'Sair do Sistema'])
+    if resposta == 1:
+        cabecalho('Opção 1')
+    elif resposta == 2:
+        cabecalho('Opção 2')
+    elif resposta == 3:
+        cabecalho('Saindo do sistema... Até logo!')
+        break
+    else:
+        cabecalho('\033[31mERRO! Digite uma opção válida.\033[m')
+    sleep(1)
