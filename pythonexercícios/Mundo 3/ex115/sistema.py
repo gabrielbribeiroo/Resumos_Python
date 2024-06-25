e173a43ef1a38bce0ae1c48afa00d8ddfa8d5399
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2024-06-25 15:04:46 -0300

    updating menu with file in python

--- pythonexercícios/Mundo 3/ex115/sistema.py
+++ pythonexercícios/Mundo 3/ex115/sistema.py
@@ -10,11 +10,16 @@ if not arquivoExiste(arq):
 while True:
     resposta = menu(['Ver pessoas cadastradas', 'Cadastrar nova pessoa', 'Sair do Sistema'])
     if resposta == 1:
-        # Opção de listar o conteúdo de um arquivo!
+        # Opção de listar o conteúdo de um arquivo.
         lerArquivo(arq)
     elif resposta == 2:
-        cabecalho('Opção 2')
+        # Opção de cadastrar uma nova pessoa.
+        cabecalho('NOVO CADASTRO')
+        nome = str(input('Nome: '))
+        idade = leiaInt('Idade: ')
+        cadastrar(arq, nome, idade)
     elif resposta == 3:
+        # Opção de sair do sistema.
         cabecalho('Saindo do sistema... Até logo!')
         break
     else:
